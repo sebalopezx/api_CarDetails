3293e78ac07b83e76edf03ee138c80d683143154
sebalopezx <se.lopezn@gmail.com>
2024-05-08 22:39:15 -0400

    17. add config db mongo

--- server/db.js
+++ server/db.js
@@ -3,9 +3,12 @@ import { MONGODB_URI } from './config.js'
 
 export async function connectDB(){
     try{
-        const db = await mongoose.connect(MONGODB_URI);
+        const db = await mongoose.connect(MONGODB_URI, {        
+            useNewUrlParser: true,
+            useUnifiedTopology: true,          
+        });
         console.log("Conexion a MongoDB: ", db.connection.name)
     } catch ( error ){
-        console.error(error);
+        console.error('Error al conectar a MongoDB:', error);
     }
 };
