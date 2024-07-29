19d7f11409ce91ff2dc971c2e83430c78ba918d9
sebalopezx <se.lopezn@gmail.com>
2024-07-28 20:03:26 -0400

    23. add api urlbase

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,26 +1,32 @@
 import axios from "axios";
 
+
+const api = axios.create({
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:4000'
+});
+
+
 // Listado vehiculos
-export const getVehiculosRequest = async () => await axios.get('/vehiculos')
+export const getVehiculosRequest = async () => await api.get('/vehiculos')
 
 
 // Marcas
-export const createNewMarcaRequest = async (marca) => await axios.post('/marcas', marca)
+export const createNewMarcaRequest = async (marca) => await api.post('/marcas', marca)
 
-export const deleteMarcaRequest = async (id) => await axios.delete(`/marcas/${id}`)
+export const deleteMarcaRequest = async (id) => await api.delete(`/marcas/${id}`)
 
-export const getMarcaRequest = async (id) => await axios.get(`/marcas/${id}`)
+export const getMarcaRequest = async (id) => await api.get(`/marcas/${id}`)
 
-export const updateMarcaRequest = async (id, updMarca) => await axios.put(`/marcas/${id}`, updMarca)
+export const updateMarcaRequest = async (id, updMarca) => await api.put(`/marcas/${id}`, updMarca)
 
 
 // Modelos
-export const getModelosRequest = async (idmarca) => await axios.post(`/marcas/${idmarca}/modelos/`)
+export const getModelosRequest = async (idmarca) => await api.post(`/marcas/${idmarca}/modelos/`)
 
-export const createNewModeloRequest = async (idmarca, modelo) => await axios.post(`/marcas/${idmarca}/modelos/`, modelo)
+export const createNewModeloRequest = async (idmarca, modelo) => await api.post(`/marcas/${idmarca}/modelos/`, modelo)
 
-export const deleteModeloRequest = async (idmarca, idmodelo) => await axios.delete(`/marcas/${idmarca}/modelos/${idmodelo}`)
+export const deleteModeloRequest = async (idmarca, idmodelo) => await api.delete(`/marcas/${idmarca}/modelos/${idmodelo}`)
 
-export const getModeloRequest = async (idmarca, idmodelo) => await axios.get(`/marcas/${idmarca}/modelos/${idmodelo}`)
+export const getModeloRequest = async (idmarca, idmodelo) => await api.get(`/marcas/${idmarca}/modelos/${idmodelo}`)
 
-export const updateModeloRequest = async (idmarca, idmodelo, updModelo) => await axios.put(`/marcas/${idmarca}/modelos/${idmodelo}`, updModelo)
+export const updateModeloRequest = async (idmarca, idmodelo, updModelo) => await api.put(`/marcas/${idmarca}/modelos/${idmodelo}`, updModelo)
